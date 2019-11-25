9131fc47188b175fbe53d758ef82411b81481283
misha <homakhaker@gmail.com>
2019-11-25 18:42:20 +0100

    Added builder

--- appizza/src/App.js
+++ appizza/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Layout from './containers/Layout/Layout';
+import PizzaBuilder from './components/pizzaBuilder/PizzaBuilder';
 
 function App() {
     return (
         <div className="App">
-            
+            <Layout>
+              <PizzaBuilder/>
+            </Layout>
         </div>
     );
 }
---
+++ appizza/src/components/pizzaBuilder/PizzaBuilder.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class PizzaBuilder extends Component {
+
+    render() {
+        return (<p>hi</p>);
+    }
+}
+
+export default PizzaBuilder;
