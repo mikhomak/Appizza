77125f9d38811bb4b6be638b9e628fd2227c8b0c
misha <homakhaker@gmail.com>
2019-11-26 17:37:41 +0100

    Added css doughs and curst

--- appizza/src/components/pizzaContainer/pizza/Pizza.js
+++ appizza/src/components/pizzaContainer/pizza/Pizza.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import classes from './Pizza.module.css';
+import Aux from '../../../hoc/Aux';
 
+const pizza = (props) => {
+    return (
+        <Aux>
+            <div className={classes.Pizza}>
+                <div className={classes.Crust}/>
+                <div className={classes.Dough}/>
+                <div className={classes.DoughTwo}/>
+                <div className={classes.DoughThree}/>
+                <div className={classes.DoughFour}/>
+            </div>
+        </Aux>
 
-const pizza = (props) =>{
-    return(
-        <div className={classes.Pizza}/>
     );
 };
 
