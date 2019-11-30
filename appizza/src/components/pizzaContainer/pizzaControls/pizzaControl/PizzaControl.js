e3cbb1d2fd9d39cd7d211b86f70c06e48e8a08a9
misha <homakhaker@gmail.com>
2019-12-01 00:46:59 +0100

    bottom style

--- appizza/src/components/pizzaContainer/pizzaControls/pizzaControl/PizzaControl.js
+++ appizza/src/components/pizzaContainer/pizzaControls/pizzaControl/PizzaControl.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import classes from './PizzaControl.module.css';
 import PizzaIngredient from '../../pizza/pizzaTypes/pizzaIngredient/PizzaIngredient';
+import Aux from '../../../../hoc/Aux';
+
 const pizzaControl = (props) => {
     return (
-        <PizzaIngredient type={props.type}/>
+        <Aux>
+            <PizzaIngredient type={props.type}/>
+            <div className={classes.ButtonContainer}>
+                <button className={classes.button}>-</button>
+                <button className={classes.button}>+</button>
+            </div>
+        </Aux>
     );
 };
 
