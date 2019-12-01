d14d3ce7952d1507a784ab4ce17ca04ead8403ba
misha <homakhaker@gmail.com>
2019-12-01 17:47:33 +0100

    Added position for each ingredient on the pizza

--- appizza/src/components/pizzaContainer/pizza/pizzaTypes/PizzaTypes.js
+++ appizza/src/components/pizzaContainer/pizza/pizzaTypes/PizzaTypes.js
@@ -3,28 +3,28 @@ import PizzaIngredient from './pizzaIngredient/PizzaIngredient';
 
 const positions = {
     tomato: [
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50}
+        {posX: 60, posY: 65},
+        {posX: 25, posY: 12},
+        {posX: 68, posY: 13},
+        {posX: 3, posY: 44}
     ],
     mushroom: [
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50}
+        {posX: 35, posY: 70},
+        {posX: 75, posY: 34},
+        {posX: 32, posY: 35},
+        {posX: 50, posY: 20}
     ],
     spinach: [
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50}
+        {posX: 32, posY: 50},
+        {posX: 78, posY: 53},
+        {posX: 20, posY: 5}
     ],
     pineapple: [
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50},
-        {posX: -32, posY: 50}
+        {posX: 15, posY: 60},
+        {posX: 65, posY: 45},
+        {posX: 45, posY: 80},
+        {posX: 32, posY: 7},
+        {posX: 5, posY: 22}
     ]
 };
 
@@ -33,11 +33,12 @@ const pizzaTypes = (props) => {
     let posX, posY;
     posX = positions[props.type][props.position].posX + '%';
     posY = positions[props.type][props.position].posY + '%';
-    const style ={
+    const style = {
         top: posX.toString(),
-        left: posY.toString()
+        left: posY.toString(),
+        position: "absolute"
     };
-    return <PizzaIngredient style={style}/>
+    return <div style={style}><PizzaIngredient type={props.type}/></div>
 
 };
 
